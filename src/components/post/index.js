d36711cec33b030ae183d0d5f3c5446a322f1353
mohanmohan515 <mohangokul515@gmail.com>
2025-02-28 21:06:42 +0530

    Add Action and Like&Comment section

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -2,14 +2,18 @@ import { useRef } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
 import Image from "./image";
-
+import Actions from "./action";
+import Footer from "./footer";
 export default function Post({ content }) {
-  console.log("content", content);
+const commentInput = useRef(null)
+const handleFocus = () => commentInput.current.focus();
 
   return (
     <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
       <Header username={content.username} />
       <Image src={content.imageSrc} caption={content.caption} />
+      <Actions docId={content.docId} totalLikes={content.likes.length} likedPhoto={content.userLikedPhoto} handleFocus={handleFocus} />
+      <Footer caption={content.caption} username={content.username} />
     </div>
   );
 }
